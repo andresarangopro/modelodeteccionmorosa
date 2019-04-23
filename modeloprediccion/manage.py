6061265c9c373037f77e49067835b96db0d53a83
Andrés Felipe Arango <andresarangopro@gmail.com>
2019-04-22 22:44:45 -0500

    second commit

--- modeloprediccion/manage.py
+++ modeloprediccion/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modelo_moroso.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modeloprediccion.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- modeloprediccion/modeloprediccion/wsgi.py
+++ modeloprediccion/modeloprediccion/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for modelo_moroso project.
+WSGI config for modeloprediccion project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modelo_moroso.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modeloprediccion.settings')
 
 application = get_wsgi_application()
